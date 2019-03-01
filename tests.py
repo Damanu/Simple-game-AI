2e61dc4ce93d4be606e1bae5a169488662761136
Damanu <e.schwarzhans@gmx.at>
2019-03-01 22:57:21 +0100

    At this point two Agents can interact with commands in the console

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 #usr/bin/python
 
 import numpy as np
-
+import random
 ##################################
 # Functions #
 
@@ -15,29 +15,72 @@ def normalize(v):
 class Agent:
 
     numofagents = 0
+    gtick = 0
+    instances = set()
 
-    def __init__(self, tick, position, lp, vel, dmg):
+    def __init__(self,name,position,tick, lp, vel, dmg, att, attspeed, vtd):
         self.position = np.array(position)
         self.lp = int(lp)
         self.vel = int(vel)
         self.dmg = int(dmg)
         self.tick = int(tick)
+        self.vtd = int(vtd)
+        self.att = int(att)
+        self.attspeed = int(attspeed)
+        self.name = str(name)
+
+        Agent.numofagents += 1
+        Agent.instances.add(self)
+
     def move(self,direction):
         dist = int(round(np.linalg.norm(np.array(direction))))
         if dist <= 2 :
-            self.direction += direction
+            self.position += direction
+            print(direction)
+            print(dist)
         else:
             self.position += np.array(direction)
             self.tick += int(round(((np.linalg.norm(direction)-2)*5.0/self.vel).clip(min = 0)))
             print(direction)
-            print(5.0/self.vel)
+            print(dist)
+
+    def attack(self, target):
+        if round(np.linalg.norm(self.position - target.position)) <= 1:
+            if (random.randint(1,20) + random.randint(1,20) + self.att) > target.vtd:
+                target.lp -= self.dmg + random.randint(1,6)
+                self.tick += self.attspeed
+            else:
+                self.tick += self.attspeed
 
 
-A=Agent(0,(3,1),10,2,1)
-B=Agent(0,(0,0),10,3,1)
+A=Agent("hans",(1,1),2,10,7,2,9,5,20)
+B=Agent("franz",(2,0),5,10,7,2,9,5,30)
+run = True
+while run:
+    print(Agent.gtick)
+    for figure in Agent.instances:
+        if (figure.tick - Agent.gtick) <= 0:
+            print(figure.name)
+            action = str(input("input: "))
+            print(action)
+            if action == "attack":
+                figure.attack(eval(input("target: ")))
+            if action == "move":
+                xdir = int(input("x: "))
+                ydir = int(input("y: "))
+                figure.move((xdir,ydir))
+            if action == "look":
+                for x in Agent.instances:
+                    print(x.name,", LP: ",x.lp , ", pos: " , x.position)
+    Agent.gtick += 1
+#if action == "attack":
+#for a in Agent.instances: print(a.lp)
+#A.attack(B)
+#print(B.lp)
+#print("pos: ",A.position)
+#print("tick: ",A.tick)
+#A.move((16,16))
+#print("pos: ",A.position)
+#print("tick: ",A.tick)
 
-print("pos: ",A.position)
-print("tick: ",A.tick)
-A.move((2,0))
-print("pos: ",A.position)
-print("tick: ",A.tick)
+
